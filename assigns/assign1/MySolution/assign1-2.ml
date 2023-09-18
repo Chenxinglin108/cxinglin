49a25f45d8d3cd497231490f72fc1b8be30b94eb
Chenxinglin108 <106778686+Chenxinglin108@users.noreply.github.com>
2023-09-17 20:35:18 -0400

    Update assign1-2.ml

--- assigns/assign1/MySolution/assign1-2.ml
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -32,7 +32,24 @@ fun intrev10(n: int): int
 
 *)
 
+let chr = Char.chr
+let ord = Char.code;;
+let digit_of_char(ch: char): int =
+  let () = assert(ch >= '0') in
+    let () = assert(ch <= '9') in ord(ch) - ord('0')
 
+    let char_to_int ch =
+      Char.code ch
+    
+
+  let string_get_at(cs:string)(i0:int): char = String.get cs i0
+
+  let string_init = String.init
+  let string_length = String.length 
+  let string_cons(c0: char)(cs: string): string = 
+  string_init(string_length(cs) + 1)(
+    fun i -> if i <= 0 then c0 else string_get_at cs (i-1))
+  
 
 
 let string_merge cs1 cs2 =
@@ -43,137 +60,12 @@ let string_merge cs1 cs2 =
     | s1, "" -> s1
     | s1,s2->
                               
-      if int_of_char s1.[0] <= int_of_char s2.[0]then
-       String.make 1 s1.[0]  ^ merge (String.sub s1 1 (String.length s1 - 1)) s2 acc
+      if char_to_int (string_get_at s1 0) <= char_to_int (string_get_at s2 0)then
+      string_cons  (string_get_at s1 0) (merge (String.sub s1 1 (String.length s1 - 1)) s2 acc)
       else
-        String.make 1 s2.[0] ^ merge s1 (String.sub s2 1 (String.length s2 - 1)) acc
+        string_cons  ( string_get_at s2 0 ) ( merge s1 (String.sub s2 1 (String.length s2 - 1)) acc)
   in
   merge cs1 cs2 "" ;;
-
-
-
-
-(* ****** ****** *)
-
-(*
-assign1-2: 10 points
-Given two ordered strings cs1 and cs2, please use
-string_make_fwork to implement string_merge(cs1, cs2)
-which returns the order string obtained from merging
-cs1 and cs2.
-
-For instance, if cs1 = "135" and cs2 = "2468", then
-string_merge(cs1)(cs2) equals "1234568"
-
-For instance, if cs1 = "abcde" and cs2 = "1234", then
-string_merge(cs1)(cs2) equals "1234abcde"
-*)
-
-(* ****** ****** *)
-
-
-
-
-
-
-(*
-assign1-3: 10 points
-A 3-letter sequence abc is 132-like
-if a < c < b holds. For instance, 123 is
-not 132-like; but 596 is 132-like.
-
-A string is 132-avoid if there is no subsequence
-abc in this string that is 132-like. Note that a
-subsequence of a string may contain NON-CONSEQUTIVE
-letters of the string.
-
-For instance, 123456789 is 132-avoid;
-For instance, 987654321 is 132-avoid;
-For instance, 123465789 is not 132-avoid since the
-subsequence 165 is 132-like.
-
-Please implement a function string_avoid_132 that
-check if a given string is 132-avoid; the function
-returns true if and only if the given string is 132-
-avoid.
-
-fun string_avoid_132(cs: string): bool
-*)
-
-(* ****** ****** *)
-
-(*
-assign1-4: 20 points
-Given two strings ds1 and ds2 of digits, please
-implement intrep_add(ds1)(ds2) that returns the
-string representing the sum of the two integers
-represented by ds1 and ds2. Note that the returned
-string should contain no leading zeros. (And we
-use the empty string to represent the number zero).
-
-fun intrep(ds1: int)(ds2: int): int					     
-
-For instance, "6123" + "987" = "7110"
-
-Note that ds1 and ds2 can be arbitrarily long. Thus,
-converting ds1 and ds2 to integers can cause overflow.
-*)
-
-(* ****** ****** *)
-
-(*
-assign1-5: 20 points
-
-A sequence of chars is ascending if any char in
-the sequence is less than or equal to the following
-one (when the following one does exist).
-Given a string cs, please implement a function
-that find the longest ascending subsequence of [cs].
-If there are more than one such sequences, the left
-most one should be returned.
-
-fun string_longest_ascend(xs: string): string
-
-For instance, given "1324561111", the function
-string_longest_ascend returns "13456"
-
-For instance, given "1234561111", the function
-string_longest_ascend returns "123456"
-
-For instance, given "1234511111", the function
-string_longest_ascend returns "111111".
-*)
-
-(* ****** ****** *)
-
-(*
-assign1-6: 20 bonus points
-A 4-letter sequence abcd is 1324-like
-if a < c < b < d holds. For instance, 1234 is
-not 132-like; but 2547 is 1324-like.
-
-A string is 1324-avoid if there is no subsequence
-abc in this string that is 1324-like. Note that a
-subsequence of a string may contain NON-CONSEQUTIVE
-letters of the string.
-
-For instance, 123456789 is 1324-avoid;
-For instance, 987654321 is 1324-avoid;
-For instance, 123465789 is not 132-avoid since the
-subsequence 1657 is 1324-like.
-
-Please implement a function string_avoid_1324 that
-check if a given string is 1324-avoid; the function
-returns true if and only if the given string is 1324-
-avoid.
-
-fun string_avoid_1324(cs: string): bool
-*)
-
-(* ****** ****** *)
-
-(* end of [CS320-2023-Fall-assigns-assign1.ml] *)
-
 
 
 
