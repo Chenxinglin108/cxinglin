8f67d19d664770edcd9f512ff5efd2811985ba01
cxinglin <CXLlxc2020!>
2023-09-19 17:22:55 -0400

    78

--- exams/quiz1/MySolution/quiz1-6.ml
+++ exams/quiz1/MySolution/quiz1-6.ml
@@ -3,7 +3,7 @@
 (* Question 6: 10 points *)
 
 let pp x y = fun f -> f(x,y)
-let ff  = fun f x y-> f(x-y) 
+let ff = fun (x, y) -> x - y
 
 (* Give an implementation of ff that
    makes the following assertions pass *)
