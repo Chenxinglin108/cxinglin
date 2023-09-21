979c5a5539322ece2369451600720ea437643232
cxinglin <CXLlxc2020!>
2023-09-20 20:57:39 -0400

    2

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -44,17 +44,18 @@ let int_to_char n =
           )
 
 
+          let c (str1:string) (str2:string) : string =
+            let length1 = string_length str1 in
+            let length2 = string_length  str2 in
+            let combined_length = length1 + length2 in
+            let combined_string = string_init combined_length (fun i ->
+              if i < length1 then
+                string_get (str1, i)
+              else
+                string_get (str2, (i - length1))
+            ) in combined_string
+
 let int2str n =
   if n<0 then c ("-") (int1 (n* (-1))) else
 int1 n
         
-  let c (str1:string) (str2:string) : string =
-    let length1 = string_length str1 in
-    let length2 = string_length  str2 in
-    let combined_length = length1 + length2 in
-    let combined_string = string_init combined_length (fun i ->
-      if i < length1 then
-        string_get (str1, i)
-      else
-        string_get (str2, (i - length1))
-    ) in combined_string
