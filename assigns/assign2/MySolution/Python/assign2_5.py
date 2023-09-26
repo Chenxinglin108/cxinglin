41b1f7970cb9722cdee55d92294277b981c072ea
cxinglin <CXLlxc2020!>
2023-09-26 17:06:07 -0400

    0

--- assigns/assign2/MySolution/Python/assign2_5.py
+++ assigns/assign2/MySolution/Python/assign2_5.py
@@ -88,17 +88,6 @@ def flist_rmake_fwork(fwork):
 
 
 
-assert(fnlist_make_fwork(lambda work: work(0)).cons1 == 0)
-assert(fnlist_make_fwork(lambda work: work(1)).cons1 == 1)
-assert(fnlist_make_fwork(lambda work: work(2)).cons1 == 2)
-assert(fnlist_make_fwork(lambda work: work(3)).cons1 == 3)
-assert(fnlist_make_fwork(lambda work: work(4)).cons1 == 4)
-####################################################
-def fwork1(work):
-    for i in range(10):
-        work(i)
-####################################################
-for i, x in enumerate(fnlist_reverse(fnlist_make_fwork(fwork1))):
-    assert(i == 10 - 1 - x)
+
 
 
