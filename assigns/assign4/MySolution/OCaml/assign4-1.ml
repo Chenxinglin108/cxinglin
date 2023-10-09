f3c93b76587232cbdcdeb6aa470c432a0079dfa5
cxinglin <CXLlxc2020!>
2023-10-09 15:20:12 -0400

    093

--- assigns/assign4/MySolution/OCaml/assign4-1.ml
+++ assigns/assign4/MySolution/OCaml/assign4-1.ml
@@ -12,7 +12,7 @@ type 'a strcon =
 type 'a stream =
 unit -> 'a strcon (* thunk *)
 
-let ln2_series_stream : float stream = 
+let the_ln2_stream : float stream = 
   let rec partial_sum_generator n sum =
     let next_term = if n mod 2 = 0 then 1.0 /. float (n + 1) else -.1.0 /. float (n + 1) in
     let next_sum = sum +. next_term in
