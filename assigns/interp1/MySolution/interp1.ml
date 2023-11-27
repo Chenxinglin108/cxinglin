4b483d18cfd2bab679cde18a16ff417bdb767a31
cxinglin <CXLlxc2020!>
2023-11-26 21:30:11 -0500

    3

--- assigns/interp1/MySolution/interp1.ml
+++ assigns/interp1/MySolution/interp1.ml
@@ -12,7 +12,7 @@ Notes:
 
 *)
 type ('a, 'e) result = Ok of 'a | Error of 'e
-type value = Int of int | Bool of bool | Unit
+type value = Int of int | Bool of bool | Unit 
 type command = Push of value | Pop | Trace | Add | Sub | Mul | Div | And | Or | Not | Lt | Gt
 
 exception Panic
@@ -50,12 +50,73 @@ let
       ord(string_get(cs, i0-1)) - ord('0')in
       helper(string_length(cs))
 
+      let is_valid_integer str =
+        let str_len = String.length str in
+        if str_len = 0 then false
+        else
+          let rec is_digit i =
+            if i >= str_len then true
+            else if i = 0 && str.[i] = '-' && str_len > 1 then is_digit (i + 1)
+            else if str.[i] >= '0' && str.[i] <= '9' then is_digit (i + 1)
+            else false
+          in
+          is_digit 0
+
+
+      
+
+let split_on_char delimiter str =
+  let rec aux i j =
+    if j >= string_length str then
+      if i >= j then [] else [String.sub str i (j - i)]
+    else if String.get str j = delimiter then
+      if i = j then aux (j + 1) (j + 1)
+      else String.sub str i (j - i) :: aux (j + 1) (j + 1)
+    else aux i (j + 1)
+  in
+  aux 0 0
+
+let string_get(cs, i0) = String.get cs i0
+
+
+
+
+let
+  str2int
+  (cs: string): int =
+  if string_get (cs,0) == '-' then let cs = String.sub cs 1 ((string_length cs)-1) in let result= let rec
+    helper(i0: int): int =
+        if i0 <= 0 then 0 else
+        10 * helper(i0 - 1) + 
+        ord(string_get(cs, i0-1)) - ord('0')in
+        helper(string_length(cs)) in -1 * result
+  else 
+  let rec
+  helper(i0: int): int =
+      if i0 <= 0 then 0 else
+      10 * helper(i0 - 1) + 
+      ord(string_get(cs, i0-1)) - ord('0')in
+      helper(string_length(cs))
+
+      let is_valid_integer str =
+        let str_len = String.length str in
+        if str_len = 0 then false
+        else
+          let rec is_digit i =
+            if i >= str_len then true
+            else if i = 0 && str.[i] = '-' && str_len > 1 then is_digit (i + 1)
+            else if str.[i] >= '0' && str.[i] <= '9' then is_digit (i + 1)
+            else false
+          in
+          is_digit 0
+      
 let parse_value str =
   match str with
   | "True" -> Bool true
   | "False" -> Bool false
   | "Unit" -> Unit
-  | _ -> Int (str2int str)
+  | _ ->Int (str2int str)
+
 
 let parse_command str =
   let parts = String.split_on_char ' ' str in
@@ -156,6 +217,42 @@ let eval_program commands =
   | Ok (_, trace) -> Some  (trace)
   | Error _ -> Some (["Panic"])
 
-let interp program_str =
-  let commands = parse_program program_str in
-  eval_program commands
+
+
+
+
+
+
+  let remove_whitespaces str =
+    let is_whitespace c = 
+      c = ' ' || c = '\n' || c = '\r' || c = '\t'
+    in 
+    list_foldleft (String.to_seq str |> List.of_seq) "" (fun acc c ->
+      if is_whitespace c then acc else acc ^ String.make 1 c
+    ) 
+  
+  let clean_string_list str_list =
+    list_map  str_list remove_whitespaces
+  
+  
+
+    let is_valid_command cmd =
+      let cmd = String.trim cmd in
+      match cmd with
+      ""->true
+      | "Pop" | "Trace" | "Add" | "Sub" | "Mul" | "Div" | "And" | "Or" | "Not" | "Lt" | "Gt"  -> true
+      | _ ->
+        if String.length cmd > 4 && String.sub cmd 0 4 = "Push" then
+          let rest = String.trim (String.sub cmd 4 (String.length cmd - 4)) in
+          is_valid_integer rest || rest = "true" || rest = "false"
+        else
+          false
+    
+  
+  let are_all_valid_commands cmd_list =
+   list_foldleft cmd_list true(fun acc cmd -> acc && is_valid_command cmd) 
+
+   let interp program_str = if program_str == "" then Some[] else if (are_all_valid_commands(clean_string_list(String.split_on_char ';' program_str))) == true then
+    let commands = parse_program program_str in
+    eval_program commands else None
+   
